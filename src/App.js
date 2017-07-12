4fb83b8b814f57f5ce2ba65595231193f2d7db01
Shamim <shamim.jahandideh@gmail.com>
2017-07-12 15:30:05 +0430

    adding a sample react component - todolist - just the boilerplate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
+import TodoList from './TodoList';
 
 class App extends Component {
   render() {
@@ -8,10 +11,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>My TODOs</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <TodoList />
         </p>
       </div>
     );
---
+++ src/TodoList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class TodoList extends Component {
+  render() {
+    return (
+      <ul id='todos'>
+        <li id='1'>todo 1</li>
+        <li id='2'>todo 2</li>
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
