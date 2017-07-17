370b3dbb8fa3c496cbd1e499fb6f37f59d5f9765
Shamim <shamim.jahandideh@gmail.com>
2017-07-17 16:27:33 +0430

    adding basic routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 // style
 import './assets/styles/index.css';
 // component
 import Home from './components/home';
+import NewTask from './components/task/new-task';
 import registerServiceWorker from './registerServiceWorker';
 import configureStore from './store/configureStore';
 
@@ -13,7 +15,13 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Home />
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/new" component={NewTask} />
+      </div>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
