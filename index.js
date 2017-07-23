10c146973ac908887a4bc39f675c867bf610044f
Shamim <shamim.jahandideh@gmail.com>
2017-07-23 16:13:23 +0430

    fixing across origin issue

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // a simple todo-list app server using Express
+const APP_ENDPOINT = 'http://localhost:3000';
 
 const express = require('express');
 const app = express();
@@ -12,15 +13,26 @@ var json_body_parser = bodyParser.json();
 var urlencoded_body_parser = bodyParser.urlencoded({extended: true});
 app.use(json_body_parser);
 app.use(urlencoded_body_parser);
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.get('/', (request, response) => {
+  console.log('items: ', taskItems);
   response.json({
     data: {tasks: taskItems},
   });
 });
 
 app.get('/tasks', (request, response) => {
+  console.log('items: ', taskItems);
   response.json({
+    status: 200,
     data: {tasks: taskItems},
   });
 });
@@ -31,11 +43,12 @@ app.post('/tasks', (req, res) => {
   taskItems.push({
     id: taskItems.length,
     text: task.text,
-    tags: task.tags || []
+    tags: task.tags || [],
   });
 
+  console.log('items: ', taskItems);
   res.send({
-    data: {tasks: taskItems}
+    data: {tasks: taskItems},
   });
 });
 
